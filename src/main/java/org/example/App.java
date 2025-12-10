52580b374c1aabf88c9774cb809ee4f355afc260
korjje617 <korjje617@gmail.com>
2025-12-10 11:59:54 +0900

    Controller 상속 + switch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,57 +3,69 @@ package org.example;
 import java.util.Scanner;
 
 public class App {
-
     public void run() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== Article Manager ===");
+        System.out.println("==프로그램 시작==");
 
-        articleController articleController = new articleController(sc);
-        memberController memberController = new memberController(sc);
+        MemberController memberController = new MemberController(sc);
+        ArticleController articleController = new ArticleController(sc);
 
         articleController.makeTestData();
-        memberController.makeUserData();
+        memberController.makeTestData();
 
+        Controller controller = null;
 
         while (true) {
             System.out.print("명령어 ) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("프로그램을 종료합니다.");
                 break;
             } else if (cmd.length() == 0) {
-                System.out.println("명령어를 입력하세요.");
+                System.out.println("명령어 입력하세요");
                 continue;
             }
 
-            if (cmd.equals("member join")) {
-                memberController.doJoin();
-            }
-            else if (cmd.equals("article write")) {
-                articleController.doWrite();
+            String[] cmdBits = cmd.split(" ");
+
+            String controllerName = cmdBits[0];
+
+            if (cmdBits.length == 1) {
+                System.out.println("명령어 확인 필요");
+                continue;
             }
 
-            else if (cmd.startsWith("article list")) {
-                articleController.viewList(cmd);
+            String actionMethodName = cmdBits[1];
+
+            if (controllerName.equals("article")) {
+                controller = articleController;
+            } else if (controllerName.equals("member")) {
+                controller = memberController;
+            } else {
+                System.out.println("지원하지 않는 기능");
+                continue;
             }
 
-            else if (cmd.startsWith("article detail")) {
-                articleController.viewDetail(cmd);
-            }
+            controller.doAction(cmd, actionMethodName);
 
-            else if (cmd.startsWith("article delete")) {
-                articleController.doDelete(cmd);
-            }
-
-            else if (cmd.startsWith("article modify")) {
-                articleController.doModify(cmd);
-            }
-
-            else {
-                System.out.println("사용 할 수 없는 명령어입니다");
-            }
+//            if (cmd.equals("member join")) {
+//                memberController.doJoin();
+//            } else if (cmd.equals("article write")) {
+//               articleController.doWrite();
+//            } else if (cmd.startsWith("article list")) {
+//               articleController.showList(cmd);
+//            } else if (cmd.startsWith("article detail")) {
+//              articleController.showDetail(cmd);
+//            } else if (cmd.startsWith("article delete")) {
+//                articleController.doDelete(cmd);
+//            } else if (cmd.startsWith("article modify")) {
+//              articleController.doModify(cmd);
+//            } else {
+//                System.out.println("사용할 수 없는 명령어입니다");
+//            }
         }
+        System.out.println("==프로그램 끝==");
+        sc.close();
     }
 }
